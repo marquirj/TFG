2e9e9878aa66b82892f16454524016682fa72f6e
marquirj <juanmartinquiros@correo.ugr.es>
2020-10-13 19:03:37 +0200

    Añadir carpeta

---
+++ src/Jugador.js
@@ -0,0 +1,29 @@
+class Jugador {
+    constructor(id, nombre, posicion, puntuacion){
+        this.id = id;
+        this.nombre = nombre;
+        this.posicion = posicion;
+        this.puntuacion = puntuacion;
+    }
+
+    get nombre(){
+        return this.nombre;
+    }
+    get posicion(){
+        return this.posicion;
+    }
+    get puntuacion(){
+        return this.puntuacion;
+    }
+
+    set nombre(nombre){
+        this.nombre = nombre;
+    }
+    set posicion(pos){
+        this.posicion = pos;
+    }
+    set puntuacion(punt){
+        this.puntuacion = punt;
+    }
+
+}

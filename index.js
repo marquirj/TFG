ad08d144f1238db32bbc46027d57549b43173ba8
marquirj <juanmartinquiros@correo.ugr.es>
2020-10-25 12:18:55 +0100

    cambio ejemplo prueba nodemon close #23

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res, next){
-    res.send("hello world")
+    res.send("Gestión de peñas")
 })
 
 const server = app.listen(8000, function(){
